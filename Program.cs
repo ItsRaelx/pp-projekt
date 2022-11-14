c58f0b8ae06919e7b7773b9c29d41472eff642f6
Dawid Iwański <kontakt@raelx.pl>
2022-11-14 19:55:42 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,25 +9,3 @@ MongoClient dbClient = new MongoClient(env["MONGODB"]);
 
 var database = dbClient.GetDatabase (env["MONGO_DATABASE"]);
 var collection = database.GetCollection<BsonDocument> (env["MONGO_COLLECTION"]);
-
-// var document = new BsonDocument { 
-//     { "student_id", 1 }, 
-//     {"scores", new BsonArray {}}, 
-//     { "class_id", 480 }
-// };
-
-// collection.InsertOne(document);
-
-// Console.Write("Type: ");
-// string type = Console.ReadLine();
-// Console.Write("Score: ");
-// double score = double.Parse(Console.ReadLine());
-// var arrayFilter = Builders<BsonDocument>.Filter.Eq("student_id", 1);
-// var arrayUpdate = Builders<BsonDocument>.Update.Push<BsonDocument>("scores", new BsonDocument { { "type", type }, { "score", score } });
-// collection.UpdateOne(arrayFilter , arrayUpdate);
-// Console.WriteLine("Done!");
-
-var filter = Builders<BsonDocument>.Filter.Eq("student_id", 1);
-var studentDocument = collection.Find(filter).FirstOrDefault();
-
-Console.WriteLine(studentDocument["scores"]);
